ceca3cee53ee634529c07b187ad6cd12994d4cbd
Macey Earle <maceye@Maceys-Air.public.axo.com>
2022-04-22 13:22:47 -0400

    working artists table

--- grammys.py
+++ grammys.py
@@ -18,7 +18,7 @@ def get_links():
    song_scrape = soup.find_all('div', class_ = 'x-text song-title')
    artist_scrape = soup.find_all('div', class_ = 'x-text artist-name')
    year_scrape = soup.find_all('div', class_ = 'x-text song-rel')
-   #songs = []
+   songs = []
    #artists = []
    for index in range(len(song_scrape)):
        a = (song_scrape[index])
@@ -30,80 +30,73 @@ def get_links():
        
        index +=1
        tup = (song,artist,year[-4:])
-   return tup
- 
+       songs.append(tup)
+   return songs
+
+
+
 # Creating database
 def createDatabase(db_name):
    path = os.path.dirname(os.path.abspath(__file__))
    conn = sqlite3.connect(path+'/'+db_name)
    cur = conn.cursor()
    return cur, conn
- 
+
+
 #create table for artist and artistid
-def create_artist_table(data):
+def create_artist_table(artistnames):
    cur, conn = createDatabase('artists.db')
    cur.execute("CREATE TABLE IF NOT EXISTS artists (artistid INTEGER UNIQUE PRIMARY KEY, artistname STRING)")
-   
+
+   for i in range(len(artistnames)):
+      print(i)
+      print(artistnames[i])
+      cur.execute("INSERT INTO artists (artistid, artistname) VALUES (?,?)",(i, artistnames[i]))
+      conn.commit()
+
+ 
+# gets a list of artists only once 
+
+def get_artistnames(data):
    artist_lst = []
    for result in data:
-      if result in artist_lst:
-         print(result)
+      if result[1] in artist_lst:
          continue
       else:
          artist_lst.append(result[1])
-      print(artist_lst)
-   for i in range(len(artist_lst)):
-      print(i)
-      print(artist_lst[i])
-      cur.execute("INSERT INTO artists (artistid, artistname) VALUES (?,?)",(i,artist_lst[i]))
-      conn.commit()
+   return artist_lst
 
-#cur, conn = createDatabase('artists')  
-#create_artist_table()
- 
-
-# gets a list of artists only once 
-
-# def insert_artistsdata(data):
-#    artist_lst = []
-#    for result in data:
-#       if result[1] in artist_lst:
-#          continue
-#       else:
-#          artist_lst.append(result[1])
-#    for item in range(len(artist_lst)):
-#       artistid = artist_lst[index] 
-#       artistname = item
-
-
-   #loop over the len of the list 
-   #index is the id and the item is the name 
+#loop over the len of the list 
+#index is the id and the item is the name 
    #set text as primary key 
    #TEXT PRIMARY KEY -- artist name 
    #Insert or Ignore into -- prevents duplicates 
 
-   
-# print(insert_artistsdata(get_links()))
-# print(len(insert_artistsdata(get_links())))
 
  
- 
-# #create table for grammy songs of the year
+#create table for top 100 songs of all time
 # def create_songdata_table(cur, conn):
-#    cur.execute("CREATE TABLE IF NOT EXISTS songdata (entryid INTEGER UNIQUE PRIMARY KEY IDENTITY (1,1), songtitle STRING, year INTEGER, artistid INTEGER")
+#    cur.execute("CREATE TABLE IF NOT EXISTS songdata (entryid INTEGER UNIQUE PRIMARY KEY IDENTITY, songtitle STRING, year INTEGER, artistid INTEGER")
 #    conn.commit()
  
 # def insert_songdata(results, cur, conn):
 #    for lst in results:
 #        songtitle = lst['Song']
 #        year = lst['Year']
-#        cur.execute("INSERT OR IGNORE INTO songdata(songtitle, year, artistid) VALUES (?, ?, ?, ?)"(songtitle, year, #artistid???))
+#        cur.execute("INSERT OR IGNORE INTO songdata(songtitle, year, artistid) VALUES (?, ?, ?, ?)"(songtitle, year, artistid))
 #        conn.commit()
  
  
- 
-#get_links()
 
 
-create_artist_table(get_links())
+def main():
+   songinfo = get_links() 
+   artistnames = get_artistnames(songinfo)
+   create_artist_table(artistnames)
+
+
+main()
+
+
+
 
